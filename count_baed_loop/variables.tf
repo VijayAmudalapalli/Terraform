0df3079410b050e828c40e64ad321b90fc5128e6
VijayAmudalapalli <vijmalyadri@gmail.com>
2025-10-15 22:01:06 +0530

    terraform

---
+++ count_baed_loop/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = [ "mongodb", "mysql", "redis", "rabbitmq" ] 
+}
+
+variable "zone_id" {
+    default = "Z0679561DX1FS39T6595"
+}
+
+variable "domain" {
+    default = "amudalapalli.shop"
+}
